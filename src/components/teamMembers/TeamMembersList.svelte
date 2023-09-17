b059aa8a9a251ba8fe274275a162dd9071937c23
Abdelhak <a.nassseur.dev@gmail.com>
2023-09-17 23:26:13 +0100

    comments

--- src/components/teamMembers/TeamMembersList.svelte
+++ src/components/teamMembers/TeamMembersList.svelte
@@ -2,14 +2,17 @@
     import { team } from "../../db";
 	import TeamMemberCard from "./TeamMemberCard.svelte";
     import { Button } from "flowbite-svelte";
-    import { fade, slide, scale } from "svelte/transition";
+    import { fade } from "svelte/transition";
     import { flip } from 'svelte/animate'
-	import TeamMembers from "../sections/TeamMembers.svelte";
 
-    let count = 3
+    // Declaring the initial team list count
+    let count: number = 3
+
+    // The $ sign bind one value to another reactivly when one change the other re-renders
     $:teamMembers = team.slice(0, count);
 
-    const handleShowMore = () => {
+    // Small pagination handler that will show team members two at a time if all team shown function hides back the team is reduced to initial count
+    const handleShowMore = (): void => {
         count = count + 2;
         if(teamMembers.length == team.length){
             count = 3;
@@ -20,10 +23,11 @@
 
 
 <div class=" my-12">
+    <!-- Added the teamMember id as a key, this will make every item in the dom unique -->
     {#each teamMembers as member (member.id)}   
-        <div  in:fade animate:flip={{ duration: 500}}>
+        <div in:fade animate:flip={{ duration: 500}}>
             <TeamMemberCard 
-            image={member.image}
+                image={member.image}
                 fullName={member.fullName}
                 email={member.email}
                 hours={member.hours}
@@ -37,6 +41,7 @@
             class="hover:bg-orange-400 border-orange-400"
             on:click={handleShowMore}
         >
+            <!-- Button will show content depending on the length on the team array -->
             {teamMembers.length == team.length? "Hide back" : 'Show more'}
         </Button>
     </div>
