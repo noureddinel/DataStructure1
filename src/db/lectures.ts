2fb76455edd8b4788e30949ac0463fe3655cbfd5
lnoureddine <lnoureddine4@gmail.com>
2023-09-25 16:16:33 +0100

    fixe the lecture and schedule table content

--- src/db/lectures.ts
+++ src/db/lectures.ts
@@ -1,18 +1,26 @@
 const lectures = [
     {
         id: 1,
-        time: 'Sun., Mon. and Wed. [10:40 - 12:40]',
+        time: '	Mon. [08:30 - 10:00]',
         section: 'S02',
-        place: 'Amphi',
-        staff: 'Kamel Boukhalfa'
+        place: 'Amphi 5',
+        staff: 'Fouzia Anguel'
     },
     {
         id: 2,
-        time: '	Mon. and Wed. [08:30 - 10:30], Thu. [10:40-12:40]',
-        section: 'S01',
-        place: 'Amphi',
+        time: 'Mon. [10:10 - 11:40]',
+        section: 'S03',
+        place: 'Amphi 5',
         staff: 'Noureddine Lasla'
     },
+    {
+        id: 3,
+        time: '	Mon. [11:50 - 13:20]',
+        section: 'S01',
+        place: 'Amphi 6',
+        staff: 'Noureddine Lasla'
+    },
+    
 ]
 
 export { lectures }
