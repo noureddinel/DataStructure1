1ff44d016197b4431d0a15bca5fba68d9d1ee4b9
lnoureddine <lnoureddine4@gmail.com>
2024-01-12 20:20:53 +0100

    Add lecture 14

--- src/db/schedule.ts
+++ src/db/schedule.ts
@@ -160,7 +160,7 @@ const schedule = [
         topic: 'Dynamic Memory Allocation: Application',
         sub_topic: 'Array of pointers;Linked List',
         topicDownload: 'PDF',
-        chapters: '8',
+        lecture: 'https://drive.google.com/file/d/1pjFMj-h-Jk4XHrgDJ8JiEOtoxiZx3uUz/view?usp=drive_link',
         lab: 'https://drive.google.com/file/d/1m81B5dLbuw-ObeAAtGfCCEBMDtRDgPrz/view?usp=drive_link',
         tutorials: 'tuts 5'
     },
